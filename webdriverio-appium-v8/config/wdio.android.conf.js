a9958b2ca3853e63ce1c57394443254b5d581eed
Luis Perez <tito@Luiss-MacBook-Air.local>
2023-07-13 10:49:46 -0700

    adding new test suite

--- webdriverio-appium-v8/config/wdio.android.conf.js
+++ webdriverio-appium-v8/config/wdio.android.conf.js
@@ -10,17 +10,30 @@ config.path = '/wd/hub';
 config.specs = [
     // ToDo: define location for spec files here
     // './test/specs/ios*.js'
-    '../test/specs/android/*.js'
+    // '../test/specs/android/*.js'
+    '../test/specs/android/api-demo.spec.js'
 ];
 
 // Capabilities
 config.capabilities = [
     {
+        // platformName: 'Android',
+        // 'appium:platformVersion': '11.0',
+        // 'appium:deviceName': 'Pixel 3',
+        // 'appium:automationName': 'UIAutomator2',
+        // 'appium:app': path.join(process.cwd(), './app/android/ColorNote+Notepad.apk'),
+        // "appium:autoGrantPermissions": true,
+        // platformName: 'Android',
+        // 'appium:platformVersion': '11.0',
+        // 'appium:deviceName': 'Pixel 3',
+        // 'appium:automationName': 'UIAutomator2',
+        // 'appium:app': path.join(process.cwd(), './app/android/android-calculator2.apk'), 
+        // "appium:autoGrantPermissions": true,
         platformName: 'Android',
         'appium:platformVersion': '11.0',
         'appium:deviceName': 'Pixel 3',
         'appium:automationName': 'UIAutomator2',
-        'appium:app': path.join(process.cwd(), './app/android/ColorNote+Notepad.apk'),
+        'appium:app': path.join(process.cwd(), './app/android/ApiDemos-debug.apk'), 
         "appium:autoGrantPermissions": true,
     }
 ];
---
+++ webdriverio-appium-v8/test/specs/android/api-demo.spec.js
@@ -0,0 +1,33 @@
+describe('Api demo app', () => {
+    it('Game Controller Input', async() => {
+        await $('~Views').click();
+
+        await $('//android.widget.TextView[@content-desc="Game Controller Input"]').click();
+
+        const description = $('//android.widget.TextView[@resource-id="io.appium.android.apis:id/description"]'); 
+        await expect(description).toHaveText('This activity demonstrates how to process input events received from game controllers. Please connect your game controller now and try moving the joysticks or pressing buttons. If it helps, try to imagine that you are a lone space cowboy in hot pursuit of the aliens who kidnapped your favorite llama on their way back to Andromeda...');
+
+        await expect(('//*[@resource-id="io.appium.android.apis:id/game"]')).not.toBeExisting();
+        await driver.back();
+        await driver.back();
+    });
+
+    it('Adding text', async() => {
+        await $('~Text').click();
+
+        await $('//*[@text="KeyEventText"]').click();
+        await $('//*[@resource-id="io.appium.android.apis:id/text"]').addValue('Today is a great day');
+
+        const text = $('//android.widget.TextView[@resource-id="io.appium.android.apis:id/text"]')
+        await expect(text).toHaveText('Today is a great day');
+
+        await $('//android.widget.Button[@content-desc="Clear"]').click();
+        await driver.back();
+        await driver.back();
+    });
+
+    // it('', async() => {
+    //     await $('~').click();
+
+    // });
+});
