fc47ab294418c5318824499685e22a26ed23fe01
Luis Perez <68307398+LuisP-coder@users.noreply.github.com>
2023-08-03 09:38:39 -0700

    Update ios-findElements.spec.js

--- webdriverio-appium-v8/test/specs/ios-findElements.spec.js
+++ webdriverio-appium-v8/test/specs/ios-findElements.spec.js
@@ -1,7 +1,9 @@
 describe('ios Find Element', () => {
     it('find element by accessibility Id', async() => {
-        await $('~Alert Views').click();
-        await $('~Simple').click();
+        await $('~Alert Views')
+            .click();
+        await $('~Simple')
+            .click();
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
     });
 
@@ -10,22 +12,28 @@ describe('ios Find Element', () => {
         // await $('//XCUIElementTypeStaticText[@name="Alert Views"]').click();
         // await $('//XCUIElementTypeStaticText[@label="Simple"]').click();
 
-        await $('//*[@name="Alert Views"]').click();
-        await $('//*[label="Simple"]').click();
+        await $('//*[@name="Alert Views"]')
+            .click();
+        await $('//*[label="Simple"]')
+            .click();
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
     });
 
     it('find element by class chain', async() => {
         const alertText = '**/XCUIElementTypeStaticText[`label CONTAINS "Alert Views"`]';
-        await $(`-ios class chain:${alertText}`).click();
-        await $('//*[@label="Simple"]').click();
+        await $(`-ios class chain:${alertText}`)
+            .click();
+        await $('//*[@label="Simple"]')
+            .click();
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best");   
     });
 
     it.only('find element by predictable string', async() => {
         const alertText = 'label == "Alert Views"';
-        await $(`-ios predicate string:${alertText}`).click();
-        await $('//*[@label="Simple"]').click();
+        await $(`-ios predicate string:${alertText}`)
+            .click();
+        await $('//*[@label="Simple"]')
+            .click();
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
     });
 });
